53daac0bcecfa426d97260148255c0b6c027f69f
HewaliGogoi <45413335+HewaliGogoi@users.noreply.github.com>
2022-03-25 12:20:16 +0530

    changes made

---
+++ unit-5/employee_form/src/components/Employee.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import EmployeeForm from './EmployeeForm';
+
+const Employee = () => {
+
+  return (
+    <>
+    <EmployeeForm/>
+    </>
+  )
+}
+
+export default Employee;
