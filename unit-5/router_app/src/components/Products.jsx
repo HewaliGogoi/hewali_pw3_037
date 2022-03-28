fec22776dd6850e93341dcbef577efbb90439291
HewaliGogoi <45413335+HewaliGogoi@users.noreply.github.com>
2022-03-28 22:23:54 +0530

    Products

--- unit-5/router_app/src/components/Products.jsx
+++ unit-5/router_app/src/components/Products.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const ProductWrapper = styled.div`
+  border:2px solid black;
+
+  a{
+    margin: 2%;
+  }
+`;
 
 const Products = () => {
+  
   return (
-    <div>Products</div>
+    <ProductWrapper>
+      <div>Products</div>
+      <a href="">Electronics</a>
+      <a href="">Clothing</a>
+      <a href="">Food</a>
+      <a href="">Stationary</a>
+    </ProductWrapper>
   )
 }
 
