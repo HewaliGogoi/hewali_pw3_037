76ce0b4cb028fa8b03dd5aebc326ff116632c238
HewaliGogoi <45413335+HewaliGogoi@users.noreply.github.com>
2022-04-14 17:51:12 +0530

    simple calculator using module export

---
+++ unit-6/simpleCalculator/main.js
@@ -0,0 +1,9 @@
+const addition = require('./add');
+const subtraction = require('./subtract');
+const multiply = require('./multiply');
+const divide = require('./divide');
+
+console.log(addition.add(1,2));
+console.log(subtraction.sub(2,1));
+console.log(multiply.mul(2,2));
+console.log(divide.div(6,2));
